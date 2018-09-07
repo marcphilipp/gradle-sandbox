85e0e662aee2c3afa187761dcbb6bc343e6aa1d8
Marc Philipp <marc@gradle.com>
2018-09-07 09:29:50 +0200

    Add Spock sample that uses Assumptions

---
+++ spock-assumption-failure/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    groovy
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    compile("org.codehaus.groovy:groovy-all:2.4.15")
+    testCompile("org.spockframework:spock-core:1.0-groovy-2.4")
+}

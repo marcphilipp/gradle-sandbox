f92d12413e04e0fc97febd134f0f3826408db54d
Marc Philipp <marc@gradle.com>
2019-10-20 16:09:34 +0200

    Add Spring boot sample project with BOM resolution conflict

---
+++ junit-bom-spring-dm-integration-issue/src/test/java/com/example/demogradle/DemoGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demogradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

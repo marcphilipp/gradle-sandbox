3685b11cefeba2374a2304da2a1ba5457b65e6b6
Marc Philipp <marc@gradle.com>
2020-06-15 11:02:05 +0200

    Add maven-checkstyle-override-property-expansion sample

---
+++ maven-checkstyle-override-property-expansion/src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+package org.example;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Prints "Hello World!".
+     *
+     * @param args program arguments
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ maven-checkstyle-override-property-expansion/src/main/java/org/example/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Example package.
+ */
+package org.example;

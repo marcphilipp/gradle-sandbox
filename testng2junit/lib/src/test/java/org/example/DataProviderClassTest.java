80f69e633c645911fae5f71b6e37bdcae2a797e7
Marc Philipp <marc@gradle.com>
2020-10-14 08:13:50 +0200

    Use ArgumentsSource instead of MethodSource

--- testng2junit/lib/src/test/java/org/example/DataProviderClassTest.java
+++ testng2junit/lib/src/test/java/org/example/DataProviderClassTest.java
@@ -1,23 +1,28 @@
 package org.example;
 
+import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import java.util.stream.Stream;
 
 public class DataProviderClassTest {
 
     @ParameterizedTest
-    @MethodSource("org.example.DataProviderClassTest$StaticProvider#createData1")
+    @ArgumentsSource(StaticProvider.class)
     public void verifyData1(String n1, Integer n2) {
         System.out.println(n1 + " " + n2);
     }
 
-    public static class StaticProvider {
-
-        public static Object[][] createData1() {
-            return new Object[][]{
-                    {"Cedric", 36},
-                    {"Anne", 37},
-            };
+    public static class StaticProvider implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+            return Stream.of(
+                    Arguments.of("Cedric", 36),
+                    Arguments.of("Anne", 37)
+            );
         }
     }
 }

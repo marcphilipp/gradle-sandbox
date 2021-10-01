6e40d70a5f8dfd119e3f446904f66c8c1cb50ad2
Marc Philipp <marc@gradle.com>
2021-10-01 14:55:39 +0200

    Make it break with Gradle 5.0

--- test-discovery-error/lib/build.gradle.kts
+++ test-discovery-error/lib/build.gradle.kts
@@ -11,6 +11,9 @@ dependencies {
     testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.8.1") {
         exclude(group = "junit", module = "junit")
     }
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.8.1") {
+        because("otherwise Gradle uses the bundled version")
+    }
 }
 
 tasks.test {
